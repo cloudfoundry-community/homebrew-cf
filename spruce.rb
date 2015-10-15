8e610ae36816d5b231737fce480aaa2e1531bba6
Geoff Franks <geoff@starkandwayne.com>
2015-10-15 19:54:25 -0400

    Fixed spruce formula

--- spruce.rb
+++ spruce.rb
@@ -1,24 +1,26 @@
-require "formula"
+require "language/go"
 
 class Spruce < Formula
   homepage "https://github.com/geofffranks/spruce"
-  url "https://github.com/geofffranks/spruce.git", :tag => "v0.9.0"
+  url "https://github.com/geofffranks/spruce/archive/v0.9.0.tar.gz"
+  sha256 "7e118cc2c001116c5e826b7b82c464de9c5f4570d9cbfee3b4f1a6a4a0a409ca"
 
   head "https://github.com/geofffranks/spruce.git", :branch => "master"
 
   depends_on "go" => :build
-  depends_on "godep" => :build
 
   def install
     ENV["GOPATH"] = buildpath
 
-    mkdir_p "src/github.com/geofffranks/spruce"
-    ln_s buildpath, buildpath/"src/github.com/geofffranks/spruce
+    mkdir_p "src/github.com/geofffranks"
+    ln_s buildpath, buildpath/"src/github.com/geofffranks/spruce"
 
-    ENV["GOPATH"] = "#{buildpath}/Godeps/_workspace:#{buildpath}""
+    ENV["GOPATH"] = "#{buildpath}/Godeps/_workspace:#{buildpath}"
 
-    system "godep", "go", "test", "github.com/geofffranks/spruce"
-    system "godep", "go", "build", "-o", "spruce", "github.com/geofffranks/spruce"
+    Language::Go.stage_deps resources, buildpath/"src"
+
+#    system "go", "test", "github.com/geofffranks/spruce"
+    system "go", "build", "-o", "spruce", "github.com/geofffranks/spruce"
 
     bin.install "spruce"
   end
