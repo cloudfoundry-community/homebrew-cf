bed22f52d16956920a0cfe47b146e3c350c44528
Geoff Franks <geoff.franks@gmail.com>
2016-03-01 12:23:43 -0500

    Enabled auto-incrementing of spruce formula via CI
    The spruce CI pipeline needs to be able to give this
    formula the shasum of what it will download, prior to having
    created the github release. To make that (and install times of the formula)
    easier, switched to downloading the raw binary, and added stub
    comments for making sed less risky.

--- spruce.rb
+++ spruce.rb
@@ -1,27 +1,14 @@
-require "language/go"
+require "FileUtils"
 
 class Spruce < Formula
   homepage "https://github.com/geofffranks/spruce"
-  url "https://github.com/geofffranks/spruce/archive/v1.1.0.tar.gz"
-  sha256 "50caffffc2beb417fbe7abf94b93d6cfc4c8caf624618d03d912eec88856e9f1"
 
-  head "https://github.com/geofffranks/spruce.git", :branch => "master"
-
-  depends_on "go" => :build
+  version = "v1.1.0" # CI Managed
+  url "https://github.com/geofffranks/spruce/releases/download/#{version}/spruce-darwin-amd64"
+  sha256 "8ea821ae0a408ccba4c1a058f528031c0b0b236cb96c837237d2bf7862064591" # CI Managed
 
   def install
-    ENV["GOPATH"] = buildpath
-
-    mkdir_p "src/github.com/geofffranks"
-    ln_s buildpath, buildpath/"src/github.com/geofffranks/spruce"
-
-    ENV["GOPATH"] = "#{buildpath}/Godeps/_workspace:#{buildpath}"
-
-    Language::Go.stage_deps resources, buildpath/"src"
-
-#    system "go", "test", "github.com/geofffranks/spruce"
-    system "go", "build", "-o", "spruce", "github.com/geofffranks/spruce"
-
+    FileUtils.mv("spruce-darwin-amd64", "spruce")
     bin.install "spruce"
   end
 
