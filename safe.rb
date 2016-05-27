25f8e7da285bc65715ee7d05c2057e43b61b4a10
Geoff Franks <geoff.franks@gmail.com>
2016-05-27 08:25:05 -0700

    Added safe brew formula

---
+++ safe.rb
@@ -0,0 +1,19 @@
+require "FileUtils"
+
+class Safe < Formula
+  homepage "https://github.com/jhunt/safe"
+
+  v = "v0.0.11" # CI Managed
+  url "https://github.com/jhunt/safe/releases/download/#{v}/safe-darwin-amd64"
+  version v
+  sha256 "22baaff7c6901a531e5f2cd11f5e97507b2ce89163d32cabfeca308d1aea7561" # CI Managed
+
+  def install
+    FileUtils.mv("safe-darwin-amd64", "safe")
+    bin.install "safe"
+  end
+
+  test do
+    system "#{bin}/safe", "--version"
+  end
+end
