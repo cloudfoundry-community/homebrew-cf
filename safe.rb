bc8a2fe60f1e7ebe9cb2972c6703c776563af1a4
Geoff Franks <geoff.franks@gmail.com>
2016-05-27 09:19:14 -0700

    Intentionally breaking recipes for spruce/safe to verify pipeline fixes them

--- safe.rb
+++ safe.rb
@@ -3,10 +3,10 @@ require "FileUtils"
 class Safe < Formula
   homepage "https://github.com/jhunt/safe"
 
-  v = "v0.0.11" # CI Managed
+  v = "v0.0.10" # CI Managed
   url "https://github.com/jhunt/safe/releases/download/#{v}/safe-darwin-amd64"
   version v
-  sha256 "22baaff7c6901a531e5f2cd11f5e97507b2ce89163d32cabfeca308d1aea7561" # CI Managed
+  sha256 "22b7c6901a531e5f2cd11f5e97507b2ce89163d32cabfeca308d1aea7561" # CI Managed
 
   def install
     FileUtils.mv("safe-darwin-amd64", "safe")
--- spruce.rb
+++ spruce.rb
@@ -3,10 +3,10 @@ require "FileUtils"
 class Spruce < Formula
   homepage "https://github.com/geofffranks/spruce"
 
-  v = "v1.4.0" # CI Managed
+  v = "v1.3.0" # CI Managed
   url "https://github.com/geofffranks/spruce/releases/download/#{v}/spruce-darwin-amd64"
   version v
-  sha256 "a42b4df9078611276113b1780a9b1b5b72188eb728acbc5f468c84ec2e97733b" # CI Managed
+  sha256 "axx42b4df9078611276113b1780a9b1b5b72188eb728acbc5f468c84ec2e97733b" # CI Managed
 
   def install
     FileUtils.mv("spruce-darwin-amd64", "spruce")
