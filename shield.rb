807d75c0f6f127b1549c5d114496d004d4a52fcd
James Hunt <james@niftylogic.com>
2016-09-22 14:38:46 -0400

    Fix shield checksum

--- shield.rb
+++ shield.rb
@@ -6,7 +6,7 @@ class Shield < Formula
   v = "v0.6.6" # CI Managed
   url "https://github.com/starkandwayne/shield/releases/download/#{v}/shield-darwin-amd64"
   version v
-  sha256 "a10aaae7d680bf59407407361c351d643e84c0bf18c8cfc3988a492c1883394a" # CI Managed
+  sha256 "36bc43ccda74cc172b297b37891d377ca4ddbfb02958969c02bc7d81af28620c" # CI Managed
 
   def install
     FileUtils.mv("shield-darwin-amd64", "shield")
