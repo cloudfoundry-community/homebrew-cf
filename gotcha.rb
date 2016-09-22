6cd27bac6fbe7713f992cdd156d3228da0f6d7a1
James Hunt <james@niftylogic.com>
2016-09-22 14:35:52 -0400

    Fix gotcha checksum

--- gotcha.rb
+++ gotcha.rb
@@ -6,7 +6,7 @@ class Gotcha < Formula
   v = "v0.1.0" # CI Managed
   url "https://github.com/starkandwayne/gotcha/releases/download/#{v}/gotcha-darwin-amd64"
   version v
-  sha256 "494a1c00aa112b571e948313530ad74a40e150ab82fafd9891ff1bcc9bf9be81" # CI Managed
+  sha256 "6baf9b87b0a5d42d6be31ce82dccf56161a036fa10b4bfbe911b846a9e8f6620" # CI Managed
 
   def install
     FileUtils.mv("gotcha-darwin-amd64", "gotcha")
