5031837295acae5505f1fb2b8313de15f054b412
johnlonganecker <johnlonganecker@gmail.com>
2016-10-13 16:29:26 -0700

    no really I am removing the v from version now, add sha

--- bosh2.rb
+++ bosh2.rb
@@ -4,13 +4,14 @@ class Bosh2 < Formula
   homepage "https://github.com/cloudfoundry/bosh-cli"
 
   v = "v0.0.70" # CI Managed
-  url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{v}-darwin-amd64"
+  # remove v from version number
+  @@verNum = v.sub "v", "" 
+  url "https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-#{@@verNum}-darwin-amd64"
   version v
-  sha256 "" # CI Managed
+  sha256 "36c101150830445a2fcd72a4bd059e3f4827426b08e573792dea89c58c516a61" # CI Managed
 
   def install
-    verNum = v.sub "v", ""
-    FileUtils.mv("bosh-cli-#{verNum}-darwin-amd64", "bosh2")
+    FileUtils.mv("bosh-cli-#{@@verNum}-darwin-amd64", "bosh2")
     bin.install "bosh2"
   end
 
