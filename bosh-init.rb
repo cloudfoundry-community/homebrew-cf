da1db38289c48edb9b3df93dc60314fd0daadb86
johnlonganecker <johnlonganecker@gmail.com>
2016-10-13 22:33:04 -0700

    Revert "Updated bosh-init.rb from new release"
    This reverts commit 139c7354d524313d39f694890ca4dab19b5b93b2.

--- bosh-init.rb
+++ bosh-init.rb
@@ -8,7 +8,7 @@ class BoshInit < Formula
   @@verNum = v.sub "v", "" 
   url "https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-#{@@verNum}-darwin-amd64"
   version v
-  sha256 "" # CI Managed
+  sha256 "05f3b10c9adb78d45c367b902bec37e8379c5a044d45fde55d0a42b916a8eb40" # CI Managed
 
   def install
     FileUtils.mv("bosh-init-#{@@verNum}-darwin-amd64", "bosh-init")
