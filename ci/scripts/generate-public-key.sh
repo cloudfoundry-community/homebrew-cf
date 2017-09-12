18a639f4600e2665c1716495ce7cabaee5d06aae
Dr Nic Williams <drnicwilliams@gmail.com>
2017-09-12 10:02:54 +1000

    adding 'digest-algo SHA256' to gpg.conf to try to remove weak warning

--- ci/scripts/generate-public-key.sh
+++ ci/scripts/generate-public-key.sh
@@ -23,6 +23,7 @@ export GNUPGHOME="$(mktemp -d)"
 cat >$GNUPGHOME/gpg.conf <<EOF
 personal-digest-preferences SHA256
 cert-digest-algo SHA256
+digest-algo SHA256
 default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed
 EOF
 
