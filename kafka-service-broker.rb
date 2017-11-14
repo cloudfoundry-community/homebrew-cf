c520992e1f8e140ee2313bb5aab0db6128242c97
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-14 10:12:50 +1000

    fix bin name of kafka-service-broker

--- kafka-service-broker.rb
+++ kafka-service-broker.rb
@@ -9,7 +9,7 @@ class KafkaServiceBroker < Formula
   sha256 "f313e4cf8556e20f0faf24db1b53c557a14a1a1a2b998cd0a5d2e0a1d706a7e4" # CI Managed
 
   def install
-    FileUtils.mv("kafka-service-broker-darwin-amd64", "eden")
+    FileUtils.mv("kafka-service-broker-darwin-amd64", "kafka-service-broker")
     bin.install "kafka-service-broker"
   end
 
