99aff9f7fe602c8ce5392cf5df9adbcc666c68e1
Dr Nic Williams <drnicwilliams@gmail.com>
2018-09-21 15:23:23 -0400

    bump quaa v1.0.3

--- quaa.rb
+++ quaa.rb
@@ -2,10 +2,10 @@ class Quaa < Formula
   desc "Quickly run Cloud Foundry UAA inside Apache Tomcat"
   homepage "https://github.com/cloudfoundry/uaa"
 
-  v = "1.0.2" # CI Managed
+  v = "1.0.3" # CI Managed
   url "https://github.com/starkandwayne/quick-uaa-local/archive/v#{v}.tar.gz"
   version v
-  sha256 "c51de317600995b38af54905740fb1bde8cac171a9aa987d2f78cabfea6d65e0"
+  sha256 "cf58b7c6c40b0e0de8882301b0aa5ec08868eeccfa2cd7e8a1a5d2e09875064f"
 
   devel do
     url "https://github.com/starkandwayne/quick-uaa-local.git", using: :git
