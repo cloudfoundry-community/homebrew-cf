9a5d51c18e5f55fa34f55880bcacde90e1b3beaa
Dr Nic Williams <drnicwilliams@gmail.com>
2018-09-21 08:51:45 -0400

    bin/quaa is a wrapper script with env vars setup to reuse homebrew resources

--- quaa-tomcat.rb
+++ quaa-tomcat.rb
@@ -14,7 +14,8 @@ class QuaaTomcat < Formula
   depends_on "starkandwayne/cf/uaa-cli" => "0.0.1"
 
   resource "tomcat" do
-    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-9/v9.0.12/bin/apache-tomcat-9.0.12.tar.gz"
+    version = "9.0.12"
+    url "http://www-eu.apache.org/dist/tomcat/tomcat-9/v#{version}/bin/apache-tomcat-#{version}.tar.gz"
     sha256 "1fa3d15dcbe7b1addf03cab39b27908b9e5bc3a26ab0c268c0abcc88920f51dc"
   end
 
@@ -23,18 +24,37 @@ class QuaaTomcat < Formula
   end
 
   def install
-    warfile = Dir['*.war'].first
-    prefix.install resource("manifests")
-    FileUtils.mkdir_p "#{prefix}/operators"
-    FileUtils.cp "#{prefix}/manifests/ops-files/3-http-only.yml", "#{prefix}/operators/3-http-only.yml"
-    FileUtils.rm_rf "#{prefix}/.envrc"
+    warfile  = Dir['*.war'].first
+    share.install warfile
+
+    bosh_bin = File.join(Formula["cloudfoundry/tap/bosh-cli"].opt_bin, "bosh")
+    uaa_bin  = File.join(Formula["starkandwayne/cf/uaa-cli"].opt_bin, "uaa")
+
+    resource("manifests").stage do
+      puts "Installing quaa manifests..."
+      (share/"manifests").install Dir["*", ".versions"]
+    end
 
     resource("tomcat").stage do
       puts "Installing Apache Tomcat..."
-      libexec.install Dir["*"]
-      bin.install_symlink "#{libexec}/bin/catalina.sh" => "uaa-catalina"
+      (libexec/"tomcat").install Dir["*"]
     end
 
+    quaa = <<-SHELL
+#!/bin/bash
+
+export TOMCAT_TGZ_PATH=#{(share/warfile)}
+export BOSH_BIN=#{bosh_bin}
+export UAA_BIN=#{uaa_bin}
+export CATALINA_BIN=#{(libexec/"tomcat/bin/catalina.sh")}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+#{(share/"manifests/bin/quaa")} "$@"
+SHELL
+    bin.mkpath
+    (bin/"quaa").binwrite(quaa)
   end
 
   # plist_options :manual => "uaa-catalina run"
