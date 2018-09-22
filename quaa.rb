9c751b59745fd3ec16277228aea69b115cde0933
Dr Nic Williams <drnicwilliams@gmail.com>
2018-09-22 13:45:58 -0400

    quaa: remove unused plist

--- quaa.rb
+++ quaa.rb
@@ -60,48 +60,4 @@ SHELL
 
     README
   end
-
-  plist_options :manual => "quaa up"
-
-  def _plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Disabled</key>
-        <false/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/quaa</string>
-          <string>up</string>
-        </array>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardOutPath</key>
-        <string>#{libexec}/logs/launchd.out.log</string>
-        <key>StandardErrorPath</key>
-        <string>#{libexec}/logs/launchd.err.log</string>
-      </dict>
-    </plist>
-  EOS
-  end
-
-  test do
-    ENV["CATALINA_BASE"] = testpath
-    cp_r Dir["#{libexec}/*"], testpath
-    rm Dir["#{libexec}/logs/*"]
-
-    pid = fork do
-      exec bin/"uaa-catalina", "start"
-    end
-    sleep 3
-    begin
-      system bin/"uaa-catalina", "stop"
-    ensure
-      Process.wait pid
-    end
-    assert_predicate testpath/"logs/catalina.out", :exist?
-  end
 end
