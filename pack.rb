74b10723d2275bea31b21b02a7d15f7a581184ff
Dr Nic Williams <drnicwilliams@gmail.com>
2018-10-28 09:12:03 +1000

    pack artifacts have version in url

--- pack.rb
+++ pack.rb
@@ -4,7 +4,7 @@ class Pack < Formula
   homepage "https://github.com/buildpack/pack"
 
   v = "v0.0.5" # CI Managed
-  url "https://github.com/buildpack/pack/releases/download/#{v}/pack-macos.tar.gz"
+  url "https://github.com/buildpack/pack/releases/download/#{v}/pack-#{v}-macos.tar.gz"
   version v
   sha256 "23d647e7cec08d0452ea6d5325e09538d22683b227cb502690bd435758d4a657" # CI Managed
 
