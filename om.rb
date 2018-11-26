d42fb6d0379896a2d6bfed2a6462f4faec0b42e1
Dr Nic Williams <drnicwilliams@gmail.com>
2018-11-27 08:52:23 +1100

    ci sets version as v0.46.0 but only need 0.46.0

--- om.rb
+++ om.rb
@@ -4,8 +4,9 @@ class Om < Formula
   homepage "https://github.com/pivotal-cf/om"
 
   v = "v0.46.0" # CI Managed
-  url "https://github.com/pivotal-cf/om/releases/download/#{v}/om-darwin"
-  version v
+  @@verNum = v.sub "v", ""
+  url "https://github.com/pivotal-cf/om/releases/download/#{@@verNum}/om-darwin"
+  version @@verNum
   sha256 "5184ac2b3d0b83549d93b2aedd9f011b9c816bf0a711ef897d352429d07152bb" # CI Managed
 
   def install
