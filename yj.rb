cd2f0d60ff3ef7f5c58dbbacd34ebc5f7d231b2d
Dr Nic Williams <drnicwilliams@gmail.com>
2019-07-01 08:28:02 -0700

    add yj homebrew

---
+++ yj.rb
@@ -0,0 +1,19 @@
+require "FileUtils"
+
+class Safe < Formula
+  homepage "https://github.com/sclevine/yj"
+
+  v = "v4.0.0" # CI Managed
+  url "https://github.com/sclevine/yj/releases/download/#{v}/yj-macos"
+  version v
+  sha256 "cd8e3aecfa8a8ace509f3aee6ca707b25a096d542b5634c028232d6caca31c75" # CI Managed
+
+  def install
+    FileUtils.mv("yj-macos", "yj")
+    bin.install "yj"
+  end
+
+  test do
+    system "#{bin}/yj", "-h"
+  end
+end
