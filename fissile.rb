7350c895d896760fbe908cd7d471b5dfb4ca7572
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-28 20:38:41 +1000

    [fissile] fix ruby gsub/regexp

--- fissile.rb
+++ fissile.rb
@@ -4,7 +4,7 @@ class Fissile < Formula
   homepage "https://github.com/cloudfoundry-incubator/fissile"
 
   v = "v7.0.0+354.gacefcfa4" # CI Managed
-  v_safe = v.gsub("+", "%2B").gsub(%{^v}, "")
+  v_safe = v.gsub("+", "%2B").gsub(%r{^v}, "")
   url "http://cf-opensusefs2.s3.amazonaws.com/fissile/develop/fissile-#{v_safe}.darwin-amd64.tgz"
   version v
   sha256 "a0ce729dfade481b65f8770c09e08d9603e02640e123d2275dc73730d808635e" # CI Managed
