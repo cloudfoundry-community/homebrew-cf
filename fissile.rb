ca2e1de02fd067f743d4f0db023ed0e9bb3ee681
Dr Nic Williams <drnicwilliams@gmail.com>
2019-09-28 20:31:16 +1000

    add fissile homebrew

---
+++ fissile.rb
@@ -0,0 +1,20 @@
+require "fileutils"
+
+class Fissile < Formula
+  homepage "https://github.com/cloudfoundry-incubator/fissile"
+
+  v = "7.0.0+354.gacefcfa4" # CI Managed
+  v_safe = v.gsub("+", "%2B")
+  url "http://cf-opensusefs2.s3.amazonaws.com/fissile/develop/fissile-#{v_safe}.darwin-amd64.tgz"
+  version v
+  sha256 "83bafee201f1fba80aad174a731a7528d331f5dee68c80d45d42d18510d29162" # CI Managed
+
+  def install
+    FileUtils.mv("fissile-darwin-amd64", "fissile")
+    bin.install "fissile"
+  end
+
+  test do
+    system "#{bin}/fissile", "version"
+  end
+end
