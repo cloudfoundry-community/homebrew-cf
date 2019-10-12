714d6c2f9d964ca529e4b191e72de1c8ef4d6609
Dr Nic Williams <drnicwilliams@gmail.com>
2019-10-12 11:15:59 +1000

    update uaa-cli.rb for https://github.com/cloudfoundry-incubator/uaa-cli/releases

--- uaa-cli.rb
+++ uaa-cli.rb
@@ -4,12 +4,13 @@ class UaaCli < Formula
   homepage "https://github.com/cloudfoundry-incubator/uaa-cli"
 
   v = "v0.0.1" # CI Managed
-  url "https://github.com/starkandwayne/uaa-cli-releases/releases/download/#{v}/uaa-darwin-amd64"
+  @@verNum = v.sub "v", ""
+  url "https://github.com/cloudfoundry-incubator/uaa-cli/releases/download/#{v}/uaa-darwin-amd64-#{@@verNum}"
   version v
   sha256 "1cf5209270c72aa501d8643597176d01ed02a6d08cf2bca26c73167928edf533" # CI Managed
 
   def install
-    FileUtils.mv("uaa-darwin-amd64", "uaa")
+    FileUtils.mv("uaa-darwin-amd64-#{@@verNum}", "uaa")
     bin.install "uaa"
   end
 
