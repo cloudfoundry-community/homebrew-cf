30ef6af70d41bc1561ce520c0a80b6566d91f441
Dr Nic Williams <drnicwilliams@gmail.com>
2019-10-12 11:27:04 +1000

    fix url to uaa-cli download

--- uaa-cli.rb
+++ uaa-cli.rb
@@ -5,7 +5,7 @@ class UaaCli < Formula
 
   v = "v0.8.0" # CI Managed
   @@verNum = v.sub "v", ""
-  url "https://github.com/cloudfoundry-incubator/uaa-cli/releases/download/#{v}/uaa-darwin-amd64-#{@@verNum}"
+  url "https://github.com/cloudfoundry-incubator/uaa-cli/releases/download/#{@@verNum}/uaa-darwin-amd64-#{@@verNum}"
   version v
   sha256 "d63bf174f37ae4af375a287c97127c7d8c0d3fb4fae5dd6e5337b2cb524a3796" # CI Managed
 
