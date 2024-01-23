579f2a01c940872bcf2b8a3f5c126c11241290cb
Kevin Rutten <krutten@fivetwenty.io>
2024-01-23 15:32:19 -0800

    Update repo for Safe

--- safe.rb
+++ safe.rb
@@ -4,12 +4,12 @@ class Safe < Formula
   homepage "https://github.com/starkandwayne/safe"
 
   v = "v1.9.0" # CI Managed
-  url "https://github.com/cloudfoundry-community/safe/releases/download/#{v}/safe-darwin-amd64"
+  url "https://github.com/cloudfoundry-community/safe/releases/download/#{v}/safe-#{v.gsub(/v/,"")}-darwin-amd64"
   version v
   sha256 "6854eb15989f7e4bc16d231fc28a041058922fc52da92211e1df07ff502eb6a1" # CI Managed
 
   def install
-    FileUtils.mv("safe-darwin-amd64", "safe")
+    FileUtils.mv("safe-#{v.gsub(/v/,"")}-darwin-amd64", "safe")
     bin.install "safe"
   end
 
