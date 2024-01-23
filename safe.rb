c9961c27b2773b8ec07b80879c7b1612f9e05066
Kevin Rutten <krutten@fivetwenty.io>
2024-01-23 15:57:01 -0800

    Fix version handling in safe recipe

--- safe.rb
+++ safe.rb
@@ -1,15 +1,16 @@
 require "fileutils"
 
 class Safe < Formula
-  homepage "https://github.com/starkandwayne/safe"
+  homepage "https://github.com/cloudfoundry-community/safe"
 
   v = "v1.9.0" # CI Managed
+  @@verNum = v.sub "v", ""
   url "https://github.com/cloudfoundry-community/safe/releases/download/#{v}/safe-#{v.gsub(/v/,"")}-darwin-amd64"
   version v
   sha256 "6854eb15989f7e4bc16d231fc28a041058922fc52da92211e1df07ff502eb6a1" # CI Managed
 
   def install
-    FileUtils.mv("safe-#{v.gsub(/v/,"")}-darwin-amd64", "safe")
+    FileUtils.mv("safe-#{@@verNum}-darwin-amd64", "safe")
     bin.install "safe"
   end
 
