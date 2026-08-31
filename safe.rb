a7d3c97ed0d72826585672e35495c385bc1baedf
Wayne E. Seguin <drgao@fivetwenty.io>
2026-08-31 11:14:15 -0400

    Update safe.rb for v1.22.0

--- safe.rb
+++ safe.rb
@@ -3,11 +3,11 @@ require "fileutils"
 class Safe < Formula
   homepage "https://github.com/cloudfoundry-community/safe"
 
-  v = "v1.20.0" # CI Managed
+  v = "v1.22.0" # CI Managed
   @@verNum = v.sub "v", ""
   url "https://github.com/cloudfoundry-community/safe/releases/download/#{v}/safe-#{v.gsub(/v/,"")}-darwin-amd64"
   version v
-  sha256 "a1b8424467418cfa1d0c040a232954f56da2a41c6ee95f6477b796f1bef10da2" # CI Managed
+  sha256 "6a7c82d4fa1fa342d01d4f84f5febabd432a0908b76c2538aa8ddcf1430aa229" # CI Managed
 
   def install
     FileUtils.mv("safe-#{@@verNum}-darwin-amd64", "safe")
